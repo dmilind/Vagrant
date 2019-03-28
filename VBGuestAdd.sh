45943e69b676c1fc860f18e0d28d79125c92226f
Milind Dhoke <18506584+dmilind@users.noreply.github.com>
2019-03-28 12:41:22 -0700

    Update VBGuestAdd.sh

--- VBGuestAdd.sh
+++ VBGuestAdd.sh
@@ -6,8 +6,9 @@ set -e
 yum update -y 
 yum install wget gcc kernel-devel -y 
 cd /opt
-wget http://download.virtualbox.org/virtualbox/5.2.26/VBoxGuestAdditions_5.2.26.iso 
+wget http://download.virtualbox.org/virtualbox/5.2.26/VBoxGuestAdditions_5.2.26.iso -O VBoxGuestAdditions_5.2.26.iso
 mount VBoxGuestAdditions_5.2.26.iso -o loop /mnt 
 sh /mnt/VBoxLinuxAdditions.run --nox11
 unmount /mnt 
 rm -f VBoxGuestAdditions_5.2.26.iso
+yum clean all -y 
